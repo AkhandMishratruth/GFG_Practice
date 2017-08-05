120f9086d59d056baf8223ad779737ba05100ac8
Akhand <apratap.mishra.cer15@itbhu.ac.in>
2017-08-05 10:27:36 +0530

    a

---
+++ Dynamic Programming/Floyd_Warshall_Algorithm.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Longest_Bitonic_Subsequence {
+    public static void main(String args[]) throws Exception {
+        PrintWriter p = new PrintWriter(System.out);
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        int[][] ar = new int[n][n]
+        for(int i=0;i<n;i++)
+            for(int j =0;j<n;j++)
+                ar[i][j]=in.nextInt();
+        int[][] dist = ar.clone();
+        for(int k=0;i<n;k++){
+            for(int i =0;i<n;i++){
+                for(int j =0;j<n;j++){
+                    if(dist[i][k]+dist[k][j]<dist[i][j])
+                        dist[i][j]=dist[i][k]+dist[k][j];
+                }
+            }
+        }
+        p.flush();
+        p.close();
+    }
+}
