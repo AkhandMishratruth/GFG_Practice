e1f8d7448bda252cbe7af1d4a034f2d6d2798524
AkhandMishratruth <apratap.mishra.cer15@itbhu.ac.in>
2017-12-06 11:37:00 +0530

    Removed junk text

--- DS/Detect Loop in linked list.java
+++ DS/Detect Loop in linked list.java
@@ -1,17 +1,3 @@
-/*
-Please note that it's Function problem i.e.
-you need to write your solution in the form of Function(s) only.
-Driver Code to call/invoke your function would be added by GfG's Online Judge.*/
-
-
-/* Node is defined as 
-class Node
-    {
-        int data;
-        Node next;
-        Node(int d) {data = d; next = null; }
-    }*/
-class GfG
 {
  int detectLoop(Node head){
      
